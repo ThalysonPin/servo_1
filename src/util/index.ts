a4b9ef452cad4b8d54ec664232550a78a52921eb
Thalyson Pin <thalyson_emanuels.p@hotmail.com>
2024-06-10 23:24:14 -0400

    Initial commit

---
+++ src/util/index.ts
@@ -0,0 +1,32 @@
+import { type Whatsapp } from '@wppconnect-team/wppconnect';
+
+export function splitMessages(text: string): string[] {
+  const pattern = /(?:http[s]?:\/\/[^\s]+)|(?:www\.[^\s]+)|[^.?!]+(?:[?!]+["']?|$)/g;
+  const matches = text.match(pattern);
+  return matches ?? [];
+}
+
+
+
+export async function sendMessagesWithDelay({
+  messages,
+  client,
+  targetNumber,
+}: {
+  messages: string[];
+  client: Whatsapp;
+  targetNumber: string;
+}): Promise<void> {
+  for (const [, msg] of messages.entries()) {
+    const dynamicDelay = msg.length * 100;
+    await new Promise((resolve) => setTimeout(resolve, dynamicDelay));
+    client
+      .sendText(targetNumber, msg.trimStart())
+      .then((result) => {
+        console.log('Mensagem enviada:', result.body);
+      })
+      .catch((erro) => {
+        console.error('Erro ao enviar mensagem:', erro);
+      });
+  }
+}
